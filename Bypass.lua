f988550056747e477a1d209628b96a70ec112580
vaxtalastus-web <vaxtalastus@gmail.com>
2025-10-24 19:58:05 +1100

    Update Bypass.lua

--- Bypass.lua
+++ Bypass.lua
@@ -1,6 +1,6 @@
 for i, v in pairs(getgc(true)) do
 	pcall(function()
-		if (getfunctionhash or get_function_hash or get_closure_hash)(v) == "605A34235509ABBB6946D10B499B526AC72774E441C3F9BAE4B63D321C6E759347D864CE879B39864743C0EEAC444FFF" then
+		if (getfunctionhash or get_function_hash or or getclosurehash or get_closure_hash or (debug and debug.getfunctionhash))(v) == "605A34235509ABBB6946D10B499B526AC72774E441C3F9BAE4B63D321C6E759347D864CE879B39864743C0EEAC444FFF" then
 			hookfunction(v, function()
 				return task.wait(387420489)
 			end)
